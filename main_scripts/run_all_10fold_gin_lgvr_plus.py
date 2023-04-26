2ecbc632424a109c9ab2b496c02aea5f1652ed62
jsshin1991 <60010256+jsshin1991@users.noreply.github.com>
2023-04-26 09:16:00 +0900

    Update and rename run_all_10fold.py to run_all_10fold_gin_lgvr_plus.py

--- main_scripts/run_all_10fold_gin_lgvr_plus.py
+++ main_scripts/run_all_10fold_gin_lgvr_plus.py
@@ -1,17 +1,15 @@
 import os
 
-datasets = ['MUTAG', 'PTC', 'PROTEINS', 'NCI1', 'NCI109', 'IMDBMULTI', 'IMDBBINARY', 'ENZYMES']
-run_command = 'python main_10fold_experiment.py '
-args1 = '--config=configs/10fold_config.json '
+datasets = ['MUTAG', 'PROTEINS', 'PTC', 'NCI1', 'IMDBBINARY', 'IMDBMULTI']
+run_command = 'python main_scripts/main_10fold_experiment.py '
+args1 = '--config=configs/10fold_config_gin_lgvr_plus.json '
 args2 = '--dataset_name=%s'
-
 
 def main():
     os.system('pwd')
     for i in range(len(datasets)):
         command = run_command + args1 + args2 % datasets[i]
         os.system(command)
-
 
 if __name__ == '__main__':
     main()
