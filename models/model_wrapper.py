66d64d7b533de42f3568ea1e33ec41789c028142
jsshin1991 <60010256+jsshin1991@users.noreply.github.com>
2023-04-26 09:18:27 +0900

    Update model_wrapper.py

--- models/model_wrapper.py
+++ models/model_wrapper.py
@@ -1,16 +1,23 @@
 import os
-import models
 import torch
 import torch.nn.functional as F
-from models.base_model import BaseModel
-
+from models.ppgn_type import PPGN_LGVR_plus
+from models.gin_type import GIN_LGVR
 
 class ModelWrapper(object):
-    def __init__(self, config, data, ph):
+    def __init__(self, config, data, pvr):
         self.config = config
         self.data = data
-        self.model = BaseModel(config, ph).cuda()
-        # self.model = BaseModel(config).cpu()
+
+        if self.config.gin:
+            self.config.node_labels = self.data.train_graphs[0].node_features.shape[1]
+
+        if self.config.model == 'gin_lgvr':
+            self.model = GIN_LGVR(self.config, pvr, False).cuda()
+        elif self.config.model == 'gin_lgvr_plus':
+            self.model = GIN_LGVR(self.config, pvr, True).cuda()
+        else:
+            self.model = PPGN_LGVR_plus(self.config, pvr).cuda()
 
     # save function that saves the checkpoint in the path defined in the config file
     def save(self, best: bool, epoch: int, optimizer: torch.optim.Optimizer):
@@ -23,27 +30,11 @@ class ModelWrapper(object):
         print("Model saved.")
 
     # load latest checkpoint from the experiment path defined in the config file
-    def load_by_path(self, path):
-        """
-        :param best: boolean, to load best model or last checkpoint
-        :return: tuple of optimizer_state_dict, epoch
-        """
-        print("Loading...")
-        checkpoint = torch.load(path)
-        self.model.load_state_dict(checkpoint['model_state_dict'])
-        self.model.to(torch.device('cuda'))
-        print("Model loaded.")
-
-        return checkpoint['optimizer_state_dict'], checkpoint['epoch']
-
-
-    # load latest checkpoint from the experiment path defined in the config file
     def load(self, best: bool):
         """
         :param best: boolean, to load best model or last checkpoint
         :return: tuple of optimizer_state_dict, epoch
         """
-        # self.model = models.base_model.BaseModel(self.config).to('cuda')
         filename = 'best.tar' if best else 'last.tar'
         print("Loading {}...".format(filename), end=' ')
         checkpoint = torch.load(os.path.join(self.config.checkpoint_dir, filename))
@@ -53,25 +44,35 @@ class ModelWrapper(object):
 
         return checkpoint['optimizer_state_dict'], checkpoint['epoch']
 
-    def loss_and_results(self, scores, labels):
+    def loss_and_results(self, inputs, preds, labels):
         """
-        :param scores: shape NxC
+        :param inputs: shape N x input_depth x num_nodes x num_nodes
+        :param preds: shape N x C if not self.pd and shape [N x C, N x num_nodes x num_nodes] else
         :param labels: shape Nx1 for classification, shape NxC for regression (QM9)
         :return: tuple of (loss tensor, dists numpy array) for QM9
                           (loss tensor, number of correct predictions) for classification graphs
         """
+        scores = preds[0]
+        pred_adjs = preds[1]
+        if self.config.gin:
+            true_adjs = torch.cat([g.adj_mat.unsqueeze(0) for g in inputs], 0).to(pred_adjs.device)
+        else:
+            true_adjs = inputs[:, 0, :, :].squeeze(1)
+
         if self.config.dataset_name == 'QM9':
             differences = (scores-labels).abs().sum(dim=0)
             loss = differences.sum()
+            loss += F.mse_loss(pred_adjs, true_adjs)
             dists = differences.detach().cpu().numpy()
             return loss, dists
         else:
             loss = F.cross_entropy(scores, labels, reduction='sum')
+            loss += F.mse_loss(pred_adjs, true_adjs)
             correct_predictions = torch.eq(torch.argmax(scores, dim=1), labels).sum().cpu().item()
             return loss, correct_predictions
 
-    def run_model_get_loss_and_results(self, input, labels):
-        return self.loss_and_results(self.model(input), labels)
+    def run_model_get_loss_and_results(self, inputs, labels):
+        return self.loss_and_results(inputs, self.model(inputs), labels)
 
     def train(self):
         self.model.train()
